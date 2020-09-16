eb93af33a6322f851eb91a7d9094bd5192e98833
Bruno Hebert <brunohebert94@gmail.com>
2020-09-15 23:13:00 -0400

    desafio finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,13 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string,
+  value: number,
+  type: 'income'|'outcome';
+}
+
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +21,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+      const {income, outcome} = this.transactions.reduce(
+        (count: Balance, maptransaction: Transaction)=>{
+          switch(maptransaction.type){
+            case "income":
+            count.income += maptransaction.value
+            break;
+          
+            case "outcome":
+            count.outcome += maptransaction.value
+            break;
+          }
+          return count;
+        }, {
+          income: 0,
+          outcome: 0,
+          total: 0,
+          },
+      );
+      const total = income-outcome;
+      return {income, outcome, total};
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}: CreateTransactionDTO): Transaction {
+
+    const transaction = new Transaction({title, value, type});
+    this.transactions.push(transaction);
+    
+    return transaction;
   }
 }
 
